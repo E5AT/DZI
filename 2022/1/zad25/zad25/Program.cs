0d71ada6c38fdd1981527b3244f59cdcf2b331f2
E5AT <esatcraft@gmail.com>
2024-06-25 18:28:00 +0300

    Am I good?

---
+++ 2022/1/zad25/zad25/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = int.Parse(Console.ReadLine());
+            List<int> nums = new List<int>();
+            for (int j = 0; j < i; j++)
+                nums.Add(int.Parse(Console.ReadLine()));
+
+            while (true)
+            {
+                int num = nums[0];
+                Console.WriteLine($"число: {num}, брой: {nums.Count(n => n == num)}");
+                nums.RemoveAll(n => n == num);
+                if (nums.Count == 0) break;
+            }
+
+
+        }
+    }
+}

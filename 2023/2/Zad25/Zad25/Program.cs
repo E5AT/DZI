be343b0bea88b81dc212fcf4e8a7cca1ed19b79b
E5AT <esatcraft@gmail.com>
2024-06-25 17:55:02 +0300

    first commit

---
+++ 2023/2/Zad25/Zad25/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad25
+{
+    class Program
+    {
+        static bool Poliandrome(char[] num)
+        {
+            string number = "", numb = string.Join("", num); ;
+            for (int i = num.Length-1; i >= 0; i--)
+                number += num[i];
+            if ( numb== number) return true;
+            else return false;
+        }
+        static void Main(string[] args)
+        {
+            try {
+                string num = Console.ReadLine();
+                if (int.Parse(num) < 0) throw new Exception();
+                else if (Poliandrome(num.ToCharArray())) Console.WriteLine($"{num} is a poliandrome");
+                else Console.WriteLine($"{num} is not a poliandrome");
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Incorrectly entered number");
+            }
+            }
+    }
+}

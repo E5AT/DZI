27ab90872e9b105b8e640608aebb99120dcc3cbd
E5AT <esatcraft@gmail.com>
2024-06-25 18:16:45 +0300

    Solved

---
+++ 2023/1/Zad_25/Zad_25/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_25
+{
+    class Program
+    {
+        static bool zad25(int num)
+        {
+            char[] nums = num.ToString().ToCharArray();
+            for (int i = 0; i < nums.Length; i++)
+                if (nums[i] == '-') break;
+                else if (num % (nums[i] - '0') != 0) return false;
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if (zad25(num)) Console.WriteLine("Yes");
+            else Console.WriteLine("No");
+        }
+    }
+}

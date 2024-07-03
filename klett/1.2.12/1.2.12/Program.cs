70395615d806cfbea1fe888b6d311a115ab87220
E5AT <esatcraft@gmail.com>
2024-07-03 12:37:40 +0300

    Who am I?

---
+++ klett/1.2.12/1.2.12/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2._12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int SC = int.Parse(Console.ReadLine());
+            int SM = int.Parse(Console.ReadLine());
+            int TC = int.Parse(Console.ReadLine());
+            int TM = int.Parse(Console.ReadLine());
+
+            int hour = SC + TC;
+            int minute = SM + TM;
+
+            while(true)
+            {
+                if (hour >= 24) hour -= 24;
+                else if (minute >= 60) hour += 1;
+                else break;
+            }
+
+            string text = "";
+            if (hour < 10) text += "0"; text += hour + ":";
+            if (minute < 10) text += "0"; text += minute;
+
+            Console.WriteLine(text);
+            
+        }
+    }
+}
---
+++ klett/1.3.16/1.3.16/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._3._16
+{
+    class Book
+    {
+        private string title, author;
+        public double price;
+
+        public Book(string title, string author, double price)
+        {
+            if (title == "") throw new Exception();
+            if (price < 0) throw new Exception();
+
+            this.title = title;
+            this.author = author;
+            this.price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"\"{title}\" от {author}, {price}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Book> books = new List<Book>();
+            for(int i = 0; i < n; i++)
+            {
+                string answer = Console.ReadLine();
+                List<string> elements = answer.Split(' ').ToList();
+                string title = "";
+                while (true)
+                {
+                    if (elements.Count != 3)
+                    {
+                        title += elements[0] + " "; elements.RemoveAt(0);
+                    }
+                    else break;
+                }
+                title += elements[0];
+                Book book = new Book(title, elements[1], double.Parse(elements[2]));
+                books.Add(book);
+            }
+            
+            foreach(Book book in books.OrderBy(b => b.price))
+            {
+                Console.WriteLine(book.ToString());
+            }
+        }
+    }
+}

39c8cbca7be068960a30494005eaf996693dd7cc
E5AT <esatcraft@gmail.com>
2024-07-07 08:34:10 +0300

    a

---
+++ klett/1.3.10/1.3.10/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._3._10
+{
+    class Song
+    {
+        public string title, singer;
+
+        public Song()
+        {
+            title = "unknown";
+            singer = "unknown";
+        }
+        public Song(string title)
+        {
+            this.title = title;
+            singer = "unknown";
+        }
+        public Song(string title, string singer)
+        {
+            this.title = title;
+            this.singer = singer;
+        }
+        public Song(Song song)
+        {
+            title = song.title;
+            singer = song.title;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tests = new int[4];
+            Song song;
+            for(int i = 0; i < 4; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        song = new Song();
+                        tests[i] = song.title == "unknown" && song.singer == "unknown" ? 1 : 0;
+                        break;
+
+                    case 1:
+                        song = new Song("a");
+                        tests[i] = song.title == "a" && song.singer == "unknown" ? 1 : 0;
+                        break;
+
+                    case 2:
+                        song = new Song("a", "b");
+                        tests[i] = song.title == "a" && song.singer == "b" ? 1 : 0;
+                        break;
+
+                    case 3:
+                        song = new Song();
+                        Song song1 = new Song(song);
+                        tests[i] = song.title == song1.title && song.singer == song1.singer ? 1 : 0;
+                        break;
+                }
+            }
+            int j = 1;
+            foreach(int test in tests)
+                Console.WriteLine($"Test{j++}: {test}");
+        }
+    }
+}
---
+++ klett/2.6.4/2.6.4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _2._6._4
+{
+    class Student
+    {
+        public string name;
+        public double uspeh;
+        public Student(string name, double uspeh)
+        {
+            this.name = name;
+            this.uspeh = uspeh;
+        }
+        public override string ToString()
+        {
+            return $"{name} - {uspeh}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer = Console.ReadLine();
+            StreamReader r = new StreamReader($"E:\\DZI\\klett\\2.6.4\\{answer}.txt");
+            List<Student> students = new List<Student>();
+            string line = "";
+            while (true)
+            {
+                line = r.ReadLine();
+                    if (line == null) break;
+                string[] lineElements = line.Split(' ');
+                Student student = new Student(lineElements[0] +" "+ lineElements[1], double.Parse(lineElements[3]));
+                students.Add(student);
+            }
+
+            StreamWriter w = new StreamWriter($"E:\\DZI\\klett\\2.6.4\\{answer}sorted.txt");
+            foreach(Student student in students.OrderBy(s => s.uspeh).ThenBy(s => s.name).Reverse())
+                w.WriteLine(student.ToString());
+            r.Close();  w.Close();
+        }
+    }
+}

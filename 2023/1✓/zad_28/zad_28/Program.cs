d6050228e2d0e2c50d73b9d2363e118e6123289a
E5AT <esatcraft@gmail.com>
2024-07-08 14:49:52 +0300

    b

---
+++ 2023/1✓/zad_28/zad_28/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_28
+{
+    abstract class Furniture
+    {
+        private string typeProduct;
+        private double productionPrice;
+
+        public string TypeProduct
+        {
+            get { return typeProduct; }
+            set
+            {
+                if (string.IsNullOrEmpty(value)) throw new Exception("Type product empty or null");
+                else typeProduct = value;
+            }
+        }
+
+        public double ProductionPrice
+        {
+            get { return productionPrice; }
+            set
+            {
+                if (value > 0) productionPrice = value;
+                else throw new Exception("Price must be greater than 0");
+            }
+        }
+        public abstract double PriceClient();
+
+        public Furniture(string typeProduct, double productionPrice)
+        {
+            this.typeProduct = typeProduct;
+            this.productionPrice = productionPrice;
+        }
+    }
+
+    class Table : Furniture
+    {
+        public override double PriceClient()
+        {
+            return ProductionPrice + (ProductionPrice * 0.2);
+        }
+
+        public override string ToString()
+        {
+            return $"The table costs {PriceClient():f2} lv";
+        }
+
+        public Table(string typeProduct, double productionPrice) : base(typeProduct, productionPrice) { }
+    }
+
+    class Cabinet : Furniture
+    {
+        private int numberOfHinges;
+
+        public override double PriceClient()
+        {
+            return ProductionPrice + (ProductionPrice * 0.15) + (4.50 * numberOfHinges);
+        }
+
+        public override string ToString()
+        {
+            return $"The cabinet costs {PriceClient():f2} lv.";
+        }
+
+        public Cabinet(string typeProduct, double productionPrice, int numberOfHinges) : base(typeProduct, productionPrice)
+        {
+            this.numberOfHinges = numberOfHinges;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Furniture> furnitures = new List<Furniture>();
+            while (true)
+            {
+
+                string answer = Console.ReadLine();
+                if (answer == "END") break;
+                else
+                {
+                    string[] answerElements = answer.Split(' ');
+                    if (answerElements.Length == 2)
+                        furnitures.Add(new Table(answerElements[0], double.Parse(answerElements[1])));
+                    else furnitures.Add(new Cabinet(answerElements[0], double.Parse(answerElements[1]), int.Parse(answerElements[2])));
+                }
+            }
+
+            Console.WriteLine("All tables:");
+            foreach(Furniture furniture in furnitures)
+            {
+                if(furniture is Table)
+                    Console.WriteLine(furniture.ToString());
+            }
+
+            Console.WriteLine("All cabines:");
+            foreach (Furniture furniture in furnitures)
+            {
+                if (furniture is Cabinet)
+                    Console.WriteLine(furniture.ToString());
+            }
+        }
+    }
+}
---
+++ 2024/1/Zad26/Zad26/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            List<string> nums = new List<string>();
+            List<int> count = new List<int>();
+            for (int i = 0; i < 10; i++)
+                count.Add(0);
+
+            for (int i=a; i <= b; i++)
+            {
+                nums.Add((i).ToString());
+            }
+
+            for(int i = 0; i<nums.Count; i++)
+            {
+                for (int j = 0; j < nums[i].Length; j++)
+                    count[nums[i][j]-'0']++;
+            }
+
+            Console.WriteLine($"Digit {count.IndexOf(count.Max())} - {count.Max()} times");
+        }
+    }
+}
